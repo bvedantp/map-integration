f18ebbf2efc3f192ba97ed86f4ecb7e3e5e9525c
bvedantp <bvedantp@gmail.com>
2022-04-23 23:42:53 +0530

    Init map added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Map } from "ol";
+import Layer from "ol/layer/Layer";
+import VectorSource from "ol/source/Vector";
+import 'ol/ol.css';
+import View from 'ol/View';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import {fromLonLat, toLonLat} from 'ol/proj';
+import {ZoomToExtent, defaults as defaultControls} from 'ol/control';
+import { format } from "ol/coordinate";
+import GeoJSON from 'ol/format/GeoJSON';
+import VectorImageLayer from 'ol/layer/VectorImage';
+import mapGeoJSONdata from "./map.geojson";
+
 
 function App() {
+
+React.useEffect(()=> {
+  const map = new Map({
+    controls: defaultControls().extend([
+      new ZoomToExtent({
+        extent: [7577458.802601964, 1716750.8097641077, 9021581.180584617, 2928042.4237656747,]
+      }),
+    ]),
+    layers: [
+      new TileLayer({source: new OSM()})
+    ],
+    view: new View({
+      center: fromLonLat([77.79389109276235, 21.15863687034279]),
+      zoom: 5
+    }),
+    target: 'map-container'
+  });
+
+  map.on('click', (e)=> {
+    var lonlat  = toLonLat(e.coordinate);
+    console.log(e.coordinate)
+  })
+
+  const GeoJSONlayer = new VectorImageLayer({
+    source: new VectorSource({
+      url: mapGeoJSONdata,
+      format: new GeoJSON()
+    }),
+    visible: true,
+    title: 'GeoJSONlayer'
+  })
+
+  map.addLayer(GeoJSONlayer)
+},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="map-container" style={{width:"98vw", height:"96vh"}} ></div>
     </div>
   );
 }
